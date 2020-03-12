856c80ca006305acbdbc85a5236e9d2ba8b1a0da
Emmanuelcookey15 <Emmanuelcookey744@gmail.com>
2020-03-12 16:41:35 +0100

    Revert "First commit, where I used expresso to test the UI of main and secondary activities in isolation"
    This reverts commit 616586080e6c4326a26c3066648dcd7026aea60c.

--- ForegroundServices/app/src/main/java/com/example/foregroundservices/App.java
+++
@@ -1,6 +0,0 @@
-package com.example.foregroundservices;
-
-import android.app.Application;
-
-public class App extends Application {
-}
--- ForegroundServices/app/src/main/java/com/example/foregroundservices/ExampleService.java
+++
@@ -1,6 +0,0 @@
-package com.example.foregroundservices;
-
-import android.app.Service;
-
-public class ExampleService extends Service {
-}
